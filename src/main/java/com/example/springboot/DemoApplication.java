c3a8cf95b738d2a1b402a822034f97c92b873d6f
linghongkang <linghongkang@homedo.com>
2018-12-04 14:44:20 +0800

    外部Tomcat启动，日志目录错误

--- src/main/java/com/example/springboot/DemoApplication.java
+++ src/main/java/com/example/springboot/DemoApplication.java
@@ -2,14 +2,22 @@ package com.example.springboot;
 
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.boot.builder.SpringApplicationBuilder;
+import org.springframework.boot.web.servlet.support.SpringBootServletInitializer;
 
 /**
+ * 外部Tomcat启动继承SpringBootServletInitializer
  * @author linghongkang
  */
 @SpringBootApplication
-public class DemoApplication {
+public class DemoApplication extends SpringBootServletInitializer {
 
     public static void main(String[] args) {
         SpringApplication.run(DemoApplication.class, args);
+    }
+
+    @Override
+    protected SpringApplicationBuilder configure(SpringApplicationBuilder builder) {
+        return builder.sources(DemoApplication.class);
     }
 }
